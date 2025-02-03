a6f1d9d551f274da42ab1c2ee78d24c1cd453434
Yikizi <mattiaslinholm@gmail.com>
2025-02-03 18:36:59 +0200

    made git scripts automatically detect source branch

--- start-issue.sh
+++ start-issue.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+branch=$(git branch -r | grep "origin/dev")
+[[ -n $branch ]] && branch="dev"
+[[ -z $branch ]] && branch="main"
 date +%Hh%Mmin >~/dotfiles/sway/scripts/.time
 id=$1
 name=$(glab issue ls -O json | jq '.[] | select(.iid == '"$id"') | .title')
 branchname=${name// /-} &&
   branchname=${branchname//\"/}
-glab mr create -a malinh --wip -i "$id" -b dev -y &&
+glab mr create -a malinh --wip -i "$id" -b $branch -y &&
   git fetch --prune &&
   git checkout -b "$id-${branchname,,}" origin/"$id-${branchname,,}" # must be qouted to remove the ""
--- start-work.sh
+++ start-work.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 # echo $(date) to a file to automatically track time spent
+branch=$(git branch -r | grep "origin/dev")
+[[ -n $branch ]] && branch="dev"
+[[ -z $branch ]] && branch="main"
 date +%Hh%Mmin >~/dotfiles/sway/scripts/.time
 glab issue create -a malinh -e "$2" -t "$1" -y -d "$3" &&
   id=$(glab issue ls --search "$1" --in title -F ids) &&
   branchname=${1// /-} &&
-  glab mr create -a malinh --wip -i "$id" -b dev -y &&
+  glab mr create -a malinh --wip -i "$id" -b $branch -y &&
   git fetch --prune &&
   git checkout -b "$id-$branchname" origin/"$id-$branchname"
