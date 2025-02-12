5f32b17f063b4d38a1605405b82db2ecdc690e71
Yikizi <mattiaslinholm@gmail.com>
2025-02-13 01:17:48 +0200

    must be double quotes for newline to register in nushell

--- zathura.sh
+++ zathura.sh
@@ -3,6 +3,6 @@
 file="/home/mattias/dotfiles/sway/scripts/.recent"
 
 echo "$(basename $1)" >>/home/mattias/dotfiles/sway/scripts/.recent
-/usr/bin/nu -c "open $file | split row '\n' | uniq | collect | save -f $file"
+/usr/bin/nu -c "open $file | split row "\n" | uniq | collect | save -f $file"
 
 zathura "$@"
