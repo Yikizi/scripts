9ce9b779fd4b891232b504369ad2ad6c8b65c05a
Yikizi <mattiaslinholm@gmail.com>
2025-02-16 12:56:06 +0200

    reworked running nested nushell custom commands in bash

--- quick-bookmark-add.sh
+++ quick-bookmark-add.sh
@@ -6,7 +6,8 @@ sleep 0.1
 name=$(wl-paste)
 
 grep "$name" $HOME/dotfiles/sway/scripts/.bookmarks
+echo bm add "$name" >/tmp/scriptfile
 
-[[ $? == 1 ]] && [[ -n $name ]] && nu $HOME/dotfiles/sway/scripts/bookmarks.nu add "$name" && notify-send -t 2000 "Bookmark added" && exit 0
+[[ $? == 1 ]] && [[ -n $name ]] && nu -l -c "$(cat /tmp/scriptfile)" && notify-send -t 2000 "Bookmark added" && rm /tmp/scriptfile && exit 0
 
 notify-send -t 1000 "Bookmark already exists!"
